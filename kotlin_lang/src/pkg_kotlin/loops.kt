e22dfc3061db3d145bfedd41a4dbffb1e36eee16
Droid Developer <appdev@aerogram.in>
2020-04-16 23:56:04 +0530

    loops

---
+++ kotlin_lang/src/pkg_kotlin/loops.kt
@@ -0,0 +1,67 @@
+package pkg_kotlin
+
+fun main() {
+
+    var i = 0
+    while (i <= 9) {
+        print(++i)
+    }
+
+    println("")
+
+    i = 0;
+    do {
+        print(++i)
+    } while (i <= 9)
+
+    println("")
+
+    i = 0;
+
+    for (i in 0..9) {
+        print(i)
+    }
+
+    println("")
+
+    for (i in 0 until 9) {
+        print(i)
+    }
+
+    println("")
+
+    for (i in 0 until 9 step 2) {
+        print(i)
+    }
+
+    println("")
+
+    for (i in 9 downTo 0) {
+        print(i)
+    }
+
+    println("")
+
+    val aMap = mapOf(
+        1 to "one",
+        2 to "two",
+        3 to "three"
+    )
+
+    for ((key, value) in aMap) {
+        println("$key = $value")
+    }
+
+    val aList = listOf(
+        "A",
+        "B",
+        "C"
+    )
+
+    for ((index, element) in aList.withIndex()) {
+        println("$element at $index")
+    }
+
+
+
+}
